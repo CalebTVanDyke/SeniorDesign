57eb4aa542a059c0235d3e53faf64dbec02625e8
CalebTVanDyke <ctvandyke24@gmail.com>
2015-03-27 21:44:16 -0500

    Added graph for heart rate

--- server.py
+++ server.py
@@ -15,24 +15,40 @@ app.config['SECRET_KEY'] = 'secret!'
 app.config['PROPAGATE_EXCEPTIONS'] = True
 socketio = SocketIO(app)
 oxygenThread = None
+heartThread = None
 
 
 def oxygen_thread():
-    ser = serial.Serial('COM4', 9600, timeout=1)
+    # ser = serial.Serial('COM4', 9600, timeout=1)
     while True:
         time.sleep(1)
-        x = ser.read(1)
-        val = struct.unpack('B', x)[0]
-        socketio.emit('my response',
+        # x = ser.read(1)
+        # val = struct.unpack('B', x)[0]
+        val = randint(90, 100)
+        socketio.emit('oxygen',
+                      {'data' : 'Server generated event', 'num' : val},
+                      namespace='/test')
+def heart_thread():
+    # ser = serial.Serial('COM4', 9600, timeout=1)
+    while True:
+        time.sleep(1)
+        # x = ser.read(1)
+        # val = struct.unpack('B', x)[0]
+        val = randint(60, 120)
+        socketio.emit('heart',
                       {'data' : 'Server generated event', 'num' : val},
                       namespace='/test')
 
 @app.route("/")
 def index():
     global oxygenThread
+    global heartThread
     if oxygenThread is None:
         oxygenThread = Thread(target=oxygen_thread)
         oxygenThread.start()
+    if heartThread is None:
+        heartThread = Thread(target=heart_thread)
+        heartThread.start()
     return render_template('index.html')
 
 @socketio.on('connect', namespace='/test')
