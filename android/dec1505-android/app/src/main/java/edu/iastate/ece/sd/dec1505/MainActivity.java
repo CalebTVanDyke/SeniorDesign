84c6704ca0734850a91d9524530fc66f7cc20bd7
Arlen Burroughs <arlenb@iastate.edu>
2015-10-18 22:00:40 -0500

    Added Dialog for picking a time.

--- android/dec1505-android/app/src/main/java/edu/iastate/ece/sd/dec1505/MainActivity.java
+++ android/dec1505-android/app/src/main/java/edu/iastate/ece/sd/dec1505/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
