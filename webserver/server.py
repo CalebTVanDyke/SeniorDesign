182d0c48ec87a8024cec3b987a13a1a633a34e20
CalebTVanDyke <ctvandyke24@gmail.com>
2015-10-25 16:36:45 -0500

    Changed back to port 80

--- webserver/server.py
+++ webserver/server.py
@@ -193,4 +193,4 @@ def test_disconnect():
     print('Client disconnected')
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=8080)
+    socketio.run(app, host='0.0.0.0', port=80)
