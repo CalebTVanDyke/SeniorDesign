65c5969e6cb9b73a507cf5257d65a505056a97d9
CalebTVanDyke <ctvandyke24@gmail.com>
2015-11-17 13:46:50 -0600

    Fixed pi code

--- pi/read.py
+++ pi/read.py
@@ -2,15 +2,16 @@ import serial
 import httplib
 import json
 import urllib
+import random
 
 # ser = serial.Serial('COM4', 9600, timeout=1)
 # x = ser.read(11).decode("utf-8");
-oxygen = randint(95, 100)
-heart = randint(80, 110)
-temp = uniform(97.0, 100)
+oxygen = random.randint(95, 100)
+heart = random.randint(80, 110)
+temp = random.uniform(97.0, 100)
 conn = httplib.HTTPConnection("cvandyke.ddns.net")
-query = urllib.quote(query)
-conn.request("GET", "loadData?user_id={0}&heartRate={1}&bloodOxygen={2}&temp={3}".format(user_id, heart, oxygen, temp))
+# query = urllib.quote(query)
+conn.request("GET", "loadData?user_id={0}&heartRate={1}&bloodOxygen={2}&temp={3}".format(3, heart, oxygen, temp))
 response = conn.getresponse()
 jsonStr = response.read()
 conn.close()
