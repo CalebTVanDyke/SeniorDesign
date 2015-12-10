b78f189aab067dea07565d8d72a488f234ad7aa7
CalebTVanDyke <ctvandyke24@gmail.com>
2015-12-09 23:02:57 -0600

    final updates to pi script

--- pi/read.py
+++ pi/read.py
@@ -7,9 +7,11 @@ import json
 import random
 import threading
 import sys
+import time
 
 safeData = True
 lastHeart = 90
+lastAlert = 0;
 lock = threading.Lock()
 
 def getUserInput():
@@ -27,9 +29,15 @@ def loadData():
 	threading.Timer(5.0, loadData).start()
 	global safeData
 	global lastHeart
+	global lastAlert
 	if safeData:
 		oxygen = random.randint(95, 100)
-		heart = random.randint(lastHeart-2, lastHeart+2)
+		if lastHeart <= 80:
+			heart = random.randint(80, 82)
+		elif lastHeart >= 100:
+			heart = random.randint(98, 100)
+		else:
+			heart = random.randint(lastHeart-2, lastHeart+2)
 		temp = random.uniform(97.0, 99.0)
 		lastHeart = heart
 	else:
@@ -47,9 +55,11 @@ def loadData():
 	conn.close()
 	alert = json.loads(jsonStr)
 	if alert['alert']:
-		urllib2.urlopen("http://databasesupport.arlenburroughs.com/dec1505/alert.php?user_id='3'")
-	# ser.close()
-	print "data loaded"
+		curTime = time.time()
+		if curTime - lastAlert > 600:
+			urllib2.urlopen("http://databasesupport.arlenburroughs.com/dec1505/alert.php?user_id='3'")
+			lastAlert = curTime
+			print "alert sent"
 
 loadData()
 t1 = threading.Thread(target=getUserInput)
